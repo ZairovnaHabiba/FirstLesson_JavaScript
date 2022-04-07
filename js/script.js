7fdb46e333b5d0e9ee79949a86256222f6b4d69e
ZairovnaHabiba <habibaerkinova2@gmail.com>
2022-04-07 22:23:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert ('Welcome!');
+
+let firstName = prompt('Enter your first name!');
+let lastName = prompt('Enter your last name!');
+let age = prompt('Enter your age!');
+
+alert ("Nice to meet you!");
+
+console.log(`
+    ${firstName}
+    ${lastName}
+    ${age}`);
+
+
+
